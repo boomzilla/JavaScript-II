b0d69ea7a1f5f715913e8d32ac6c4a99015d2c6c
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-03-30 10:17:13 -0700

    Task 2 complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,27 +24,58 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+//creating a testing function because I am lazy and ond't want to keep re-typing this
+function testerFunction(theResult) {
+  console.log(theResult);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, testerFunction);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]); //will error if we get an empty array, I think
+
+  //if it's really important to not error here, I could make coditional to pass null/empty to cb if arr is empty
 }
+
+last(items, testerFunction);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let theSum = x + y;
+  cb(theSum);
 }
+
+sumNums(3, 4, testerFunction);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let theProduct = x*y;
+  cb(theProduct);
 }
+
+multiplyNums(3, 4, testerFunction);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let n = 0; n < list.length; n++){
+    if (list[n] === item){
+      cb(true);
+      return; //do return, otherwise function will continue through this for loop
+    }
+  }
+  
+  cb(false);
 }
+
+contains("notHere", items, testerFunction);
+contains("Gum", items, testerFunction);
 
 /* STRETCH PROBLEM */
 
